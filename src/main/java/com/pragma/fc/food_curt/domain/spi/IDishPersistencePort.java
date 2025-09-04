118fc874460961b9b584aa8c739b4bedc354672e
jhon.hernandez <jhon.hernandez>
2025-09-04 00:54:31 -0500

    feat(domain): add updateDish method to service port and implement in use case

--- src/main/java/com/pragma/fc/food_curt/domain/spi/IDishPersistencePort.java
+++ src/main/java/com/pragma/fc/food_curt/domain/spi/IDishPersistencePort.java
@@ -4,4 +4,5 @@ import com.pragma.fc.food_curt.domain.model.Dish;
 
 public interface IDishPersistencePort {
     Dish createDish(Dish dish);
+    Dish updateDish(Integer id, Double price, String description);
 }
