0157c1b63ee0f5da790921cd8d85b464b4ce9c23
jhon.hernandez <jhon.hernandez>
2025-09-03 23:27:22 -0500

    feat: add exception handlers and bean configuration

---
+++ src/main/java/com/pragma/fc/food_curt/infraestructure/configuration/DishBeanConfiguration.java
@@ -0,0 +1,33 @@
+package com.pragma.fc.food_curt.infraestructure.configuration;
+
+import com.pragma.fc.food_curt.domain.api.IDishServicePort;
+import com.pragma.fc.food_curt.domain.spi.IDishPersistencePort;
+import com.pragma.fc.food_curt.domain.usecase.DishUseCase;
+import com.pragma.fc.food_curt.infraestructure.out.jpa.adapter.DishJpaAdapter;
+import com.pragma.fc.food_curt.infraestructure.out.jpa.mapper.IDishEntityMapper;
+import com.pragma.fc.food_curt.infraestructure.out.jpa.repository.IDishCategoryRepository;
+import com.pragma.fc.food_curt.infraestructure.out.jpa.repository.IDishRepository;
+import com.pragma.fc.food_curt.infraestructure.out.jpa.repository.IRestaurantRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class DishBeanConfiguration {
+    public final IDishRepository dishRepository;
+    public final IDishCategoryRepository dishCategoryRepository;
+    public final IRestaurantRepository restaurantRepository;
+    public final IDishEntityMapper dishEntityMapper;
+
+    @Bean
+    public IDishServicePort dishServicePort() {
+        return new DishUseCase(dishPersistencePort());
+    }
+
+    @Bean
+    public IDishPersistencePort dishPersistencePort() {
+        return new DishJpaAdapter(dishRepository, restaurantRepository, dishCategoryRepository, dishEntityMapper );
+    }
+
+}
