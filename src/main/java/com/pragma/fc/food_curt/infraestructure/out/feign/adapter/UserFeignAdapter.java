2784bb4aa99e167ba49d05a0057cd884c93161a2
jhon.hernandez <jhon.hernandez>
2025-09-04 00:02:36 -0500

    fix(feign): add null checks for safe access

--- src/main/java/com/pragma/fc/food_curt/infraestructure/out/feign/adapter/UserFeignAdapter.java
+++ src/main/java/com/pragma/fc/food_curt/infraestructure/out/feign/adapter/UserFeignAdapter.java
@@ -1,7 +1,9 @@
 package com.pragma.fc.food_curt.infraestructure.out.feign.adapter;
 
 import com.pragma.fc.food_curt.domain.spi.IUserClientPort;
+import com.pragma.fc.food_curt.infraestructure.input.rest.dto.ApiSuccess;
 import com.pragma.fc.food_curt.infraestructure.out.feign.client.IUserClientFeign;
+import com.pragma.fc.food_curt.infraestructure.out.feign.client.response.UserRoleResponseDto;
 import com.pragma.fc.food_curt.infraestructure.out.feign.entity.UserRole;
 
 public class UserFeignAdapter implements IUserClientPort {
@@ -13,7 +15,13 @@ public class UserFeignAdapter implements IUserClientPort {
 
     @Override
     public Boolean isOwner(Long documentNumber) {
-        String userRole = userClientFeign.validateUserRole(documentNumber).getPayload().getName();
-        return UserRole.OWNER.name().equals(userRole);
+        ApiSuccess<UserRoleResponseDto> response = userClientFeign.validateUserRole(documentNumber);
+
+        if (response == null || response.getPayload() == null || response.getPayload().getName() == null) {
+            return false;
+        }
+
+        return UserRole.OWNER.name().equals(response.getPayload().getName());
     }
+
 }
