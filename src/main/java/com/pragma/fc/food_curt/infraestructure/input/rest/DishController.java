568bdfeda638668dc552bdcedb54b3dbd190af0d
jhon.hernandez <jhon.hernandez>
2025-09-07 13:00:27 -0500

    feat: add getPaginatedByCategoryIdSortedByName method in controller

--- src/main/java/com/pragma/fc/food_curt/infraestructure/input/rest/DishController.java
+++ src/main/java/com/pragma/fc/food_curt/infraestructure/input/rest/DishController.java
@@ -3,7 +3,9 @@ package com.pragma.fc.food_curt.infraestructure.input.rest;
 import com.pragma.fc.food_curt.application.dto.request.CreateDishRequestDto;
 import com.pragma.fc.food_curt.application.dto.request.UpdateDishRequestDto;
 import com.pragma.fc.food_curt.application.dto.request.UpdateDishStatusRequestDto;
+import com.pragma.fc.food_curt.application.dto.response.DishListItemResponseDto;
 import com.pragma.fc.food_curt.application.dto.response.DishResponseDto;
+import com.pragma.fc.food_curt.application.dto.response.PaginationResponseDto;
 import com.pragma.fc.food_curt.application.handler.IDishHandler;
 import com.pragma.fc.food_curt.infraestructure.input.rest.dto.ApiError;
 import com.pragma.fc.food_curt.infraestructure.input.rest.dto.ApiSuccess;
@@ -12,16 +14,21 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("${app.api.prefix}/dishes")
@@ -118,6 +125,42 @@ public class DishController {
                 .status(HttpStatus.OK)
                 .body(new ApiSuccess<>(
                         "Dish status updated successfully",
+                        response
+                ));
+    }
+
+    @Operation(
+            summary = "Get all dishes sorted by name",
+            description = "Returns a paginated list of dishes sorted by name. Requires authentication.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Paginated list of dishes retrieved successfully",
+                            content = @Content(schema = @Schema(implementation = DishListItemResponseDto.class))),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = """
+                            1. Parameter 'page' < 1
+                            2. Parameter 'size' > 100
+                            """,
+                            content = @Content(schema = @Schema(implementation = ApiError.class))),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized: missing or invalid access token",
+                            content = @Content(schema = @Schema(implementation = ApiError.class))),
+            }
+    )
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping
+    public ResponseEntity<ApiSuccess<PaginationResponseDto<DishListItemResponseDto>>> getPaginatedByCategoryIdSortedByName(
+            @RequestParam @NotNull Integer page,
+            @RequestParam @NotNull Integer size,
+            @RequestParam(required = false) Integer categoryId
+    ) {
+        PaginationResponseDto<DishListItemResponseDto> response = dishHandler.getPaginatedByCategoryIdSortedByName(page, size, Optional.of(categoryId));
+        return ResponseEntity
+                .ok(new ApiSuccess<>(
+                        "Paginated list of dishes retrieved successfully",
                         response
                 ));
     }
