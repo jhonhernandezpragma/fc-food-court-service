def3e85fef65ebb88bab0bb9f1325be7a8dc5b94
jhon.hernandez <jhon.hernandez>
2025-09-07 12:56:16 -0500

    feat: add ports and getPaginatedByCategoryIdSortedByName method in use case

--- src/main/java/com/pragma/fc/food_curt/domain/api/IDishServicePort.java
+++ src/main/java/com/pragma/fc/food_curt/domain/api/IDishServicePort.java
@@ -1,9 +1,13 @@
 package com.pragma.fc.food_curt.domain.api;
 
 import com.pragma.fc.food_curt.domain.model.Dish;
+import com.pragma.fc.food_curt.domain.model.Pagination;
+
+import java.util.Optional;
 
 public interface IDishServicePort {
     Dish createDish(Dish dish, Long ownerDocumentNumber);
     Dish updateDish(Integer id, Double price, String description, Long ownerDocumentNumber);
     Dish updateDishStatus(Integer id, Boolean isActive, Long ownerDocumentNumber);
+    Pagination<Dish> getPaginatedByCategoryIdSortedByName(int page, int size, Optional<Integer> categoryId);
 }
