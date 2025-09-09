2e940cd7833492708a3f818dec14e3c44a5e34ff
jhon.hernandez <jhon.hernandez>
2025-09-09 10:05:57 -0500

    fix: add optional of nullable

--- src/main/java/com/pragma/fc/food_curt/infraestructure/input/rest/DishController.java
+++ src/main/java/com/pragma/fc/food_curt/infraestructure/input/rest/DishController.java
@@ -18,6 +18,7 @@ import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("${app.api.prefix}/dishes")
+@Validated
 public class DishController {
     private final IDishHandler dishHandler;
 
@@ -157,7 +159,7 @@ public class DishController {
             @RequestParam @NotNull Integer size,
             @RequestParam(required = false) Integer categoryId
     ) {
-        PaginationResponseDto<DishListItemResponseDto> response = dishHandler.getPaginatedByCategoryIdSortedByName(page, size, Optional.of(categoryId));
+        PaginationResponseDto<DishListItemResponseDto> response = dishHandler.getPaginatedByCategoryIdSortedByName(page, size, Optional.ofNullable(categoryId));
         return ResponseEntity
                 .ok(new ApiSuccess<>(
                         "Paginated list of dishes retrieved successfully",
