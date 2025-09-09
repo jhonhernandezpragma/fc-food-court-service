c87eac41b06cedc5c923fdcf439c0680255e0494
jhon.hernandez <jhon.hernandez>
2025-09-09 09:46:26 -0500

    feat: add jps adapter and mappers

--- src/main/java/com/pragma/fc/food_curt/infraestructure/out/jpa/repository/IOrderRepository.java
+++ src/main/java/com/pragma/fc/food_curt/infraestructure/out/jpa/repository/IOrderRepository.java
@@ -1,8 +1,14 @@
 package com.pragma.fc.food_curt.infraestructure.out.jpa.repository;
 
 import com.pragma.fc.food_curt.infraestructure.out.jpa.entity.OrderEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IOrderRepository extends JpaRepository<OrderEntity, Integer> {
     boolean existsByCustomerDocumentNumber(Long customerDocumentNumber);
+
+    Page<OrderEntity> findByStatusIdAndRestaurantNit(Integer statusId, Long restaurantNit, Pageable pageable);
+
+    Page<OrderEntity> findAllByRestaurantNit(Long restaurantNit, Pageable pageable);
 }
